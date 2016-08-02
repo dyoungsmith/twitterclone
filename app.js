e5cc0aa4953fe13f2a5ccfddb0cca273e11571bf
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-08-02 16:24:37 -0400

    actual coding

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 var express = require('express');
+var swig = require('swig');
 var app =  express();
 var port = 3000;
 
 
-// app.use(function (req, res, next) {
-// 	res.send('this is the start of the chain')
-//     next();
-// })
+app.set('views', __dirname + '/views'); // point res.render to the proper directory
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', swig.renderFile); 
+swig.setDefaults({ cache: false });
 
-app.use('/special', function(req, res) {
-	res.send('you reached the special place')
-	console.log(res.statusCode);
-	console.log('you are getting special tweets?!?');
-})
 
-app.get('/news', function(req, res) {
-	res.send('this is the NEWS get response')
-	console.log('tweet tweet');
-})
+app.get('/', function(req, res, next) {
+	res.send('under construction.');
+	next();
+});
+
+app.get('/tweets/', function(req, res, next) {
+	res.send('send us your tweets');
+	next();
+});
+
+app.post('/tweets/', function (req, res, next) {
+	// res.send('this is the start of the chain');
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+	next();
+});
 
 
 app.listen(port, function() {
 	console.log('server listening........');
-})
+});
+
